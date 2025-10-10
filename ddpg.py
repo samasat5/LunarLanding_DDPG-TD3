ca284aaf7c3b51f338e29885e02c2d837ae55b3c
Sama <Sama.Satariyan@etu.sorbonne-universite.fr>
2025-10-10 12:25:40 +0000

    Oct 10, 2025, 2:25 PM

--- ddpg.py
+++ ddpg.py
@@ -1,2 +1,23 @@
 import torch
-impo
+import gymnasium as gym
+from tensordict.nn import TensorDictModule
+from torchrl.envs import GymEnv
+from Agent import env
+from torchrl.modules import MLP
+
+module = MLP(
+    out_features=env.action_spec.shape[-1],
+    num_cells=[32, 64],
+    activation_class=torch.nn.Tanh,
+)
+
+
+module = torch.nn.LazyLinear(out_features=env.action_spec.shape[-1])
+
+policy = TensorDictModule(
+    module,
+    in_keys=["observation"],
+    out_keys=["action"],)
+
+rollout = env.rollout(max_steps=10, policy=policy)
+print(rollout)
