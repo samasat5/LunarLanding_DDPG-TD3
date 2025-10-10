202d3719e53b7daf2595a3edd3be4fa31a9eef2a
Sama <Sama.Satariyan@etu.sorbonne-universite.fr>
2025-10-10 11:38:59 +0000

    Oct 10, 2025, 1:38 PM

--- Agent.py
+++ Agent.py
@@ -12,17 +12,15 @@ from torchrl.envs.transforms import Compose
 
 env = gym.make("LunarLander-v3", render_mode="human")
 
-# Reset the environment to generate the first observation
 observation, info = env.reset(seed=42)
 for _ in range(1000):
-    # this is where you would insert your policy
+    # policy
     action = env.action_space.sample()
 
     # step (transition) through the environment with the action
     # receiving the next observation, reward and if the episode has terminated or truncated
     observation, reward, terminated, truncated, info = env.step(action)
 
-    # If the episode has ended then we can reset to start a new episode
     if terminated or truncated:
         observation, info = env.reset()
 
