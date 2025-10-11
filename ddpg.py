02b922531da7e1924381f2bd1c29bdf9225d735e
Erisa Kohansal <erisakohansal@gmail.com>
2025-10-11 19:47:00 +0000

    11 oct. 2025, 21:47

--- ddpg.py
+++ ddpg.py
@@ -1,59 +1,198 @@
+import pdb
+import time
+from copy import deepcopy
 import torch
-import gymnasium as gym
-from tensordict.nn import TensorDictModule
-from torchrl.envs import GymEnv
-from Agent import env
-from torchrl.modules import MLP
-
-import copy
-import tempfile
-
-import torch
-
-from matplotlib import pyplot as plt
-from tensordict import TensorDictBase
-
-from tensordict.nn import TensorDictModule, TensorDictSequential
-from torch import multiprocessing
-
+from torch import nn, optim
+from torchrl.envs import GymEnv, TransformedEnv, Compose, DoubleToFloat, InitTracker, ObservationNorm, StepCounter
 from torchrl.collectors import SyncDataCollector
-from torchrl.data import LazyMemmapStorage, RandomSampler, ReplayBuffer
+from torchrl.data import ReplayBuffer, SamplerWithoutReplacement, LazyTensorStorage, RandomSampler
+from torchrl.objectives import DDPGLoss, SoftUpdate
+from torchrl.modules import OrnsteinUhlenbeckProcessModule as OUNoise, MLP
+from tensordict.nn import TensorDictModule as TDM, TensorDictSequential as Seq
 
-from torchrl.envs import (
-    check_env_specs,
-    ExplorationType,
-    PettingZooEnv,
-    RewardSum,
-    set_exploration_type,
-    TransformedEnv,
-    VmasEnv,
+# parameters and hyperparameters
+INIT_RAND_STEPS = 5000 
+FRAMES_PER_BATCH = 100
+OPTIM_STEPS = 10
+EPS_0 = 0.5
+BUFFER_LEN = 1_000_000
+TARGET_UPDATE_EPS = 0.95
+REPLAY_BUFFER_SAMPLE = 128
+LOG_EVERY = 1000
+MLP_SIZE = 256
+DISCOUNT_FACTOR = 0.99
+TAU = 0.005
+
+# Seed the Python and RL environments to replicate similar results across training sessions. 
+#torch.manual_seed(0)
+
+# 1. Environment
+env = TransformedEnv(
+    GymEnv("LunarLanderContinuous-v3"),
+    Compose(
+        DoubleToFloat(),
+        InitTracker(),
+        ObservationNorm(in_keys=["observation"]),
+        StepCounter()
+        #RewardNorm(),
+    )
+)
+# env.set_seed(0)
+
+# assuming ObservationNorm is at index 2 in the Compose
+obs_norm = env.transform[2]
+obs_norm.init_stats(num_iter=1000, reduce_dim=0, cat_dim=0)
+obs_dim = env.observation_spec["observation"].shape[-1] # observation_spec : the observation space
+act_dim = env.action_spec.shape[-1] #action_spec : the action space
+
+# 2. Actor (policy)
+actor_mlp = MLP(
+    out_features=act_dim, 
+    num_cells=[MLP_SIZE, MLP_SIZE], 
+    activation_class=nn.ReLU,     # hidden activations
+    activate_last_layer=False 
+)
+actor = TDM(actor_mlp, in_keys=["observation"], out_keys=["action_raw"]) # =policy?
+# Wrap Tanh so it applies to the "action" tensor (-> still "action")
+tanh_on_action = TDM(nn.Tanh(), in_keys=["action_raw"], out_keys=["action"])
+policy = Seq(actor, tanh_on_action)   # final tanh ensures [-1, 1] range
+ou_noise = OUNoise(
+    spec=env.action_spec,
+    theta=0.15,
+    sigma=0.2,
+    dt=1e-2,
+)
+rollout_policy = Seq(policy, ou_noise)
+
+# 3. Critic (action value function)
+critic_mlp = MLP(
+    out_features=1, 
+    num_cells=[MLP_SIZE, MLP_SIZE],
+    activation_class=nn.ReLU,
+    activate_last_layer=False
+)
+critic = TDM(critic_mlp, in_keys=["observation", "action"], out_keys=["state_action_value"]) # = QValue
+
+# Target Networks
+actor_target = deepcopy(actor) # no noise in target
+critic_target = deepcopy(critic)
+
+# 4. DDPG loss module
+# --- 4) Warm-up forward to initialize lazy modules BEFORE loss/opt
+with torch.no_grad():
+    td0 = env.reset()          # has "observation"
+    _ = policy(td0.clone())    # init actor
+    td1 = td0.clone()
+    td1["action"] = env.action_spec.rand(td1.batch_size)
+    _ = critic(td1)            # init critic
+
+loss = DDPGLoss(
+    actor_network=policy, # deterministic 
+    value_network=critic,
+    loss_function="l2",
+    delay_actor=True,            
+    delay_value=True, 
 )
 
-from torchrl.modules import (
-    AdditiveGaussianModule,
-    MultiAgentMLP,
-    ProbabilisticActor,
-    TanhDelta,
-)
-from torchrl.objectives import DDPGLoss, SoftUpdate, ValueEstimators
-
-from torchrl.record import CSVLogger, PixelRenderTransform, VideoRecorder
-from tqdm import tqdm
-
-
-module = MLP(
-    out_features=env.action_spec.shape[-1],
-    num_cells=[32, 64],
-    activation_class=torch.nn.Tanh,
+# 5. Replay buffer
+replay_buffer = ReplayBuffer(
+    storage=LazyTensorStorage(max_size=BUFFER_LEN), 
+    #sampler=SamplerWithoutReplacement(),
+    sampler=RandomSampler(),
 )
 
+# 6. Collector
+collector = SyncDataCollector(
+    env,
+    rollout_policy,
+    frames_per_batch=FRAMES_PER_BATCH,
+    total_frames=50_000,
+    device="cpu",
+)
 
-module = torch.nn.LazyLinear(out_features=env.action_spec.shape[-1])
+# 7. Optimizers
+optim_actor = optim.Adam(policy.parameters(), lr=1e-4)
+optim_critic = optim.Adam(critic.parameters(), lr=1e-3)
 
-policy = TensorDictModule(
-    module,
-    in_keys=["observation"],
-    out_keys=["action"],)
+updater = SoftUpdate( # corrige???
+    loss,
+    tau=TAU,
+)
 
-rollout = env.rollout(max_steps=10, policy=policy)
-print(rollout)
+# 8. Training loop
+total_count = 0
+total_episodes = 0
+t0 = time.time()
+success_steps = []
+for i, data in enumerate(collector):
+    replay_buffer.extend(data)
+    max_length = replay_buffer[:]["next", "step_count"].max()
+    if len(replay_buffer) > INIT_RAND_STEPS:
+        for _ in range(OPTIM_STEPS):
+            if len(replay_buffer) < REPLAY_BUFFER_SAMPLE:
+                break
+            td = replay_buffer.sample(REPLAY_BUFFER_SAMPLE)
+
+            # Critic update
+            optim_critic.zero_grad(set_to_none=True)
+            loss_vals = loss(td)
+            loss_q = loss_vals["loss_value"]
+            loss_q.backward()
+            optim_critic.step()
+            updater.step()
+
+            # Actor update
+            optim_actor.zero_grad(set_to_none=True)
+            loss_vals = loss(td)
+            loss_pi = loss_vals["loss_actor"]
+            loss_pi.backward()
+            optim_actor.step()
+            updater.step()
+
+
+            """
+            # Combined update to avoid recomputing the losses
+            losses = loss(td)
+            loss_q = losses["loss_value"]
+            loss_pi = losses["loss_actor"]
+
+            optim_critic.zero_grad(set_to_none=True)
+            loss_q.backward(retain_graph=True)
+            optim_critic.step()
+            updater.step()
+
+            optim_actor.zero_grad(set_to_none=True)
+            loss_pi.backward()
+            optim_actor.step()
+            updater.step()
+            """
+
+            # Update target params
+            updater.step()
+            total_count += data.numel()
+            total_episodes += data["next", "done"].sum()
+    success_steps.append(max_length)
+
+# for i, data in enumerate(collector):
+#     replay_buffer.extend(data)
+
+
+#     batch = replay_buffer.sample(REPLAY_BUFFER_SAMPLE)
+
+#     # compute DDPG losses
+#     loss_td = loss_module(batch)
+#     actor_loss = loss_td["loss_actor"]
+#     critic_loss = loss_td["loss_qvalue"]
+
+#     # critic step
+#     optim_critic.zero_grad()
+#     critic_loss.backward()
+#     optim_critic.step()
+
+#     # actor step
+#     optim_actor.zero_grad()
+#     actor_loss.backward()
+#     optim_actor.step()
+
+#     if i % 50 == 0:
+#         print(f"Step {i}: actor_loss={actor_loss.item():.3f}, critic_loss={critic_loss.item():.3f}")
