cb8abf4381fd8c9ba385b8faa6477dcb42300c15
Sama <Sama.Satariyan@etu.sorbonne-universite.fr>
2025-10-14 18:56:51 +0000

    Oct 14, 2025, 8:56 PM

--- td3t.py
+++ td3t.py
@@ -20,19 +20,33 @@ n_actions = env.action_spec.shape[-1]
 max_action = env.action_spec.space.high[0]
 min_action = env.action_spec.space.low[0]
 
-#  Actor
+
+# Critic
 MLP_SIZE = 64
-actor_mlp = MLP(out_features=env.action_spec.shape[-1], num_cells=[MLP_SIZE, MLP_SIZE])
-actor_net = TensorDict(actor_mlp, in_keys=["observation"], out_keys=["action_value"])
-
-
-#Critic
 critic_mlp = MLP(out_features=1, num_cells=[MLP_SIZE, MLP_SIZE])
 critic_net = TensorDict(critic_mlp, in_keys=["observation", "action"], out_keys=["state_action_value"]) 
 qvalue = QValueModule(critic_net)
+
+#  Actor
+actor_mlp = MLP(out_features=env.action_spec.shape[-1], num_cells=[MLP_SIZE, MLP_SIZE])
+actor_net = TensorDict(actor_mlp, in_keys=["observation"], out_keys=["action_value"])
 
 EPS_0 = 0.2
 exploration_module = EGreedyModule(
     action_spec=env.action_spec,
     eps_init=EPS_0)
 policy = Seq(actor_net, exploration_module)
+
+# Collect the data from the agent’s interactions with the environment
+collector = SyncDataCollector(
+    env,
+    policy,
+    total_frames=10000,
+    frames_per_batch=1000,
+    max_frames_per_traj=1000,
+    device="cpu",
+)
+
+# Replay buffer
+BUFFER_LEN = 100000
+rb = ReplayBuffer(storage=LazyTensorStorage(BUFFER_LEN))
