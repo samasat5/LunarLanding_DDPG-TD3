a031c2a37fb37253c1c7a1c687cb2deebf54b409
Sama <Sama.Satariyan@etu.sorbonne-universite.fr>
2025-10-14 18:18:27 +0000

    Oct 14, 2025, 8:18 PM

--- td3t.py
+++ td3t.py
@@ -14,13 +14,20 @@ env = TransformedEnv(
     GymEnv("LunarLanderContinuous-v3"),
     StepCounter(max_steps=1000))
 env.set_seed(0)
+
 obs_dim = env.observation_spec.shape[-1]
 n_actions = env.action_spec.shape[-1]
 max_action = env.action_spec.space.high[0]
 min_action = env.action_spec.space.low[0]
 
-# Policy, Actor
+#  Actor
 MLP_SIZE = 64
 actor_mlp = MLP(out_features=env.action_spec.shape[-1], num_cells=[MLP_SIZE, MLP_SIZE])
 actor_net = TensorDict(actor_mlp, in_keys=["observation"], out_keys=["action_value"])
-policy = Seq(actor_net, QValueModule(spec=env.action_spec))
+
+
+#Critic
+critic_mlp = MLP(out_features=1, num_cells=[MLP_SIZE, MLP_SIZE])
+critic_net = TensorDict(critic_mlp, in_keys=["observation", "action"], out_keys=["state_action_value"]) 
+qvalue = QValueModule(critic_net)
+
