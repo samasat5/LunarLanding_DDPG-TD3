3ad2b3c310bf8f5e1694f01532b4e97769ced56a
Sama <Sama.Satariyan@etu.sorbonne-universite.fr>
2025-10-14 20:53:59 +0000

    Oct 14, 2025, 10:53 PM

--- td3t.py
+++ td3t.py
@@ -71,7 +71,7 @@ rb = ReplayBuffer(storage=LazyTensorStorage(BUFFER_LEN))
 
 # TD3 Loss
 loss = TD3Loss(
-    value_network=(qvalue_1, qvalue_2),  # Pass both Q-networks
+    qvalue_network=(qvalue_1, qvalue_2),  # Pass both Q-networks
     actor_network=actor_net,             # Pass the actor network
     action_space=env.action_spec,
     delay_value=True
