17bc67d3bdd29d4493e67a827ac57a5fb332e7a2
Sama <Sama.Satariyan@etu.sorbonne-universite.fr>
2025-10-14 20:34:35 +0000

    Oct 14, 2025, 10:34 PM

--- td3t.py
+++ td3t.py
@@ -27,12 +27,13 @@ min_action = env.action_spec.space.low[0]
 MLP_SIZE = 64
 critic_mlp_1 = MLP(out_features=1, num_cells=[MLP_SIZE, MLP_SIZE])
 critic_net_1 = TDM(critic_mlp_1, in_keys=["observation", "action"], out_keys=["state_action_value"]) 
-qvalue_1 = QValueModule(critic_net_1, spec=env.action_spec)  
-
+# qvalue_1 = QValueModule(critic_net_1, spec=env.action_spec)  
+qvalue_1 = critic_net_1
 
 critic_mlp_2 = MLP(out_features=1, num_cells=[MLP_SIZE, MLP_SIZE])
 critic_net_2 = TDM(critic_mlp_2, in_keys=["observation", "action"], out_keys=["state_action_value"])      
-qvalue_2 = QValueModule(critic_net_2, spec=env.action_spec)  
+# qvalue_2 = QValueModule(critic_net_2, spec=env.action_spec)  
+qvalue_2 = critic_net_2
 
 #  Actor
 actor_mlp = MLP(out_features=env.action_spec.shape[-1], num_cells=[MLP_SIZE, MLP_SIZE])
