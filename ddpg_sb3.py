10e1844e1591f0df58fc469bb4d3103e7c84c3b7
Erisa Kohansal <erisakohansal@gmail.com>
2025-10-15 19:25:50 +0000

    15 oct. 2025, 21:25

--- ddpg_sb3.py
+++ ddpg_sb3.py
@@ -4,13 +4,44 @@ from stable_baselines3 import DDPG
 from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
 
 
+# parameters and hyperparameters
+INIT_RAND_STEPS = 5000 
+TOTAL_FRAMES = 50_000
+FRAMES_PER_BATCH = 100
+OPTIM_STEPS = 10
+BUFFER_LEN = 1_000_000
+REPLAY_BUFFER_SAMPLE = 128
+LOG_EVERY = 1000
+MLP_SIZE = 256
+TAU = 0.005
+GAMMA = 0.99
+EVAL_EVERY = 10_000   # frames
+EVAL_EPISODES = 3
+DEVICE = "cpu" #"cuda:0" if torch.cuda.is_available() else "cpu"
+
+
 env = gym.make("LunarLanderContinuous-v3")
 
 # The noise objects for DDPG
 n_actions = env.action_space.shape[-1]
 action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
 
-model = DDPG("MlpPolicy", env, action_noise=action_noise, verbose=1, seed=0)
+model = DDPG(
+    "MlpPolicy", 
+    env, 
+    action_noise=action_noise, 
+    verbose=1, 
+    seed=0, 
+    learning_rate=1e-3,
+    buffer_size=BUFFER_LEN,
+    batch_size=REPLAY_BUFFER_SAMPLE,
+    tau=TAU,
+    gamma=GAMMA,
+    device=DEVICE,
+    action_noise=action_noise,
+    
+)
+
 model.learn(total_timesteps=100_000, log_interval=10_000)
 model.save("ddpg_lunarlander")
 vec_env = model.get_env()
