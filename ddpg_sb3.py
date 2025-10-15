a95864625b49cce3e7f68c9ed62fb797c53d6599
Erisa Kohansal <erisakohansal@gmail.com>
2025-10-15 18:46:33 +0000

    15 oct. 2025, 20:46

--- ddpg_sb3.py
+++ ddpg_sb3.py
@@ -11,13 +11,13 @@ n_actions = env.action_space.shape[-1]
 action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
 
 model = DDPG("MlpPolicy", env, action_noise=action_noise, verbose=1)
-model.learn(total_timesteps=10000, log_interval=10)
-model.save("ddpg_pendulum")
+model.learn(total_timesteps=50_000, log_interval=1000)
+model.save("ddpg_lunarlander")
 vec_env = model.get_env()
 
 del model # remove to demonstrate saving and loading
 
-model = DDPG.load("ddpg_pendulum")
+model = DDPG.load("ddpg_lunarlander")
 
 obs = vec_env.reset()
 while True:
