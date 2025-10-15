348b3e4eee9a8c0ba243ea24ac4e9f182a94c815
Erisa Kohansal <erisakohansal@gmail.com>
2025-10-15 11:24:53 +0200

    ddpg

--- ddpg.py
+++ ddpg.py
@@ -75,6 +75,7 @@ actor = TDM(actor_mlp, in_keys=["observation"], out_keys=["action_raw"])
 # Wrap Tanh so it applies to the "action" tensor 
 tanh_on_action = TDM(nn.Tanh(), in_keys=["action_raw"], out_keys=["action"])
 policy = Seq(actor, tanh_on_action)   # final tanh ensures [-1, 1] range
+policy = TDM(actor, in_keys=["action_raw"], out_keys=["action"])
 ou_noise = OUNoise(
     spec=env.action_spec,
     theta=0.15,
@@ -123,7 +124,7 @@ replay_buffer = ReplayBuffer(
 )
 
 # 6. Collector
-collector = SyncDataCollector(
+collector = SyncDataCollector( # renvoie des batches de transitions prêts à mettre dans le rb
     env,
     rollout_policy,
     frames_per_batch=FRAMES_PER_BATCH,
@@ -139,10 +140,11 @@ optim_critic = optim.Adam(critic.parameters(), lr=1e-3)
 total_count = 0
 total_episodes = 0
 t0 = time.time()
-success_steps = []
+success_steps, qvalues = [], []
 for i, data in enumerate(collector): # runs through the data collected from the agent’s interactions with the environment
     replay_buffer.extend(data)
     max_length = replay_buffer[:]["next", "step_count"].max()
+    pdb.set_trace()
     if len(replay_buffer) > INIT_RAND_STEPS:
         for _ in range(OPTIM_STEPS):
             if len(replay_buffer) < REPLAY_BUFFER_SAMPLE:
@@ -167,10 +169,11 @@ for i, data in enumerate(collector): # runs through the data collected from the 
 
             # Update target params
             updater.step()
+            #pdb.set_trace()
 
             total_count += data.numel()
             total_episodes += data["next", "done"].sum()
-            plt.plot([loss(td)['qvalue'].mean().item()] * len(success_steps), color='r')
+            qvalues.append(loss(td)["loss_value"].item()) # loss_q or loss(td)
     success_steps.append(max_length)
 
     if total_count % LOG_EVERY == 0:
@@ -202,12 +205,8 @@ torchrl_logger.info(
     f"solved after {total_count} steps, {total_episodes} episodes and in {t1-t0}s."
 )
 
-# plt.plot(success_steps)
-# plt.title('Successful steps over training episodes')
-# plt.xlabel('Training episodes')
-# plt.ylabel('Steps')
+plt.plot(qvalues)
 plt.figure(figsize=(10,5))
 plt.title("QValues per episode")
 plt.xlabel("QValues")
 plt.ylabel("Steps")
-plt.show()
