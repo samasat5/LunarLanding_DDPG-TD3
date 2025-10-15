b33edfaf6a230bf52c3f277707386e6b9c58d955
Erisa Kohansal <erisakohansal@gmail.com>
2025-10-15 18:54:42 +0000

    15 oct. 2025, 20:54

--- ddpg_sb3.py
+++ ddpg_sb3.py
@@ -4,14 +4,14 @@ from stable_baselines3 import DDPG
 from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
 
 
-env = gym.make("LunarLanderContinuous-v3", render_mode="rgb_array")
+env = gym.make("LunarLanderContinuous-v3")
 
 # The noise objects for DDPG
 n_actions = env.action_space.shape[-1]
 action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
 
 model = DDPG("MlpPolicy", env, action_noise=action_noise, verbose=1)
-model.learn(total_timesteps=50_000, log_interval=1000)
+model.learn(total_timesteps=100_000, log_interval=10_000)
 model.save("ddpg_lunarlander")
 vec_env = model.get_env()
 
