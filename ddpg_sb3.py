440c81fd12ae0d66c164ac3c0504e27630f428c3
Erisa Kohansal <erisakohansal@gmail.com>
2025-10-15 18:41:12 +0000

    15 oct. 2025, 20:41

--- ddpg_sb3.py
+++ ddpg_sb3.py
@@ -1,14 +1,25 @@
 import gymnasium as gym
 from stable_baselines3 import DDPG
+from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
+
 
 env = gym.make("LunarLanderContinuous-v3", render_mode="human")
-env.reset()
-print("sample action : ", env.action_space.sample())
-print("observation space shape : ", env.observation_space.shape)
-print("sample observation : ", env.observation_space.sample())
 
-for step in range(200):
-    obs, reward, done, _, _ = env.step(env.action_space.sample())
-    print(reward)
+# The noise objects for DDPG
+n_actions = env.action_space.shape[-1]
+action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
 
-env.close()
+model = DDPG("MlpPolicy", env, action_noise=action_noise, verbose=1)
+model.learn(total_timesteps=10000, log_interval=10)
+model.save("ddpg_pendulum")
+vec_env = model.get_env()
+
+del model # remove to demonstrate saving and loading
+
+model = DDPG.load("ddpg_pendulum")
+
+obs = vec_env.reset()
+while True:
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = vec_env.step(action)
+    env.render("human")
