82b7876e41c77482552bd5842a9958d0dcbd6e16
Sama <Sama.Satariyan@etu.sorbonne-universite.fr>
2025-10-15 12:40:35 +0000

    Oct 15, 2025, 2:40 PM

--- td3t.py
+++ td3t.py
@@ -92,11 +92,32 @@ BUFFER_LEN = 100000
 rb = ReplayBuffer(storage=LazyTensorStorage(BUFFER_LEN))
 
 
-# TD3 Loss
-loss = TD3Loss(
-    qvalue_network=(critic_net_1, critic_net_2),  
-    actor_network=actor_net,           
-)
+
+# test
+# Test forward with dummy batch
+dummy_obs = torch.randn(10, obs_dim)  # Batch of 10
+dummy_action = torch.randn(10, act_dim)
+
+td = TensorDict({
+    "observation": dummy_obs,
+    "action": dummy_action
+}, batch_size=[10])
+
+# Forward pass
+print("Actor output:", actor_net(td)["action"].shape)
+print("Critic 1 output:", critic_net_1(td)["state_action_value"].shape)
+print("Critic 2 output:", critic_net_2(td)["state_action_value"].shape)
+
+
+
+
+
+
+# # TD3 Loss
+# loss = TD3Loss(
+#     qvalue_network=(critic_net_1, critic_net_2),  
+#     actor_network=actor_net,           
+# )
 
 
 # # optimizer
