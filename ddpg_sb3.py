c51c27b4006c1db08c5342296d6260d339f9dac1
Erisa Kohansal <erisakohansal@gmail.com>
2025-10-15 19:22:32 +0000

    15 oct. 2025, 21:22

--- ddpg_sb3.py
+++ ddpg_sb3.py
@@ -10,7 +10,7 @@ env = gym.make("LunarLanderContinuous-v3")
 n_actions = env.action_space.shape[-1]
 action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
 
-model = DDPG("MlpPolicy", env, action_noise=action_noise, verbose=1)
+model = DDPG("MlpPolicy", env, action_noise=action_noise, verbose=1, seed=0)
 model.learn(total_timesteps=100_000, log_interval=10_000)
 model.save("ddpg_lunarlander")
 vec_env = model.get_env()
