1d50223aece23b06e4dcb1a460bb8179f85f4aff
Sama <Sama.Satariyan@etu.sorbonne-universite.fr>
2025-10-16 09:56:07 +0000

    Oct 16, 2025, 11:56 AM

--- td3new.py
+++ td3new.py
@@ -246,6 +246,7 @@ for i, data in enumerate(collector):  # Data from env rollouts
         total_count += data.numel()
         total_episodes += data["next", "done"].sum()
         qvalues.append((loss_q1.item() + loss_q2.item()) / 2) #TODO
+        pdb.set_trace()
 
     success_steps.append(max_length)
 
