3c5f364b8a972255cfb90bbcb5930d5e758cd510
Sama <Sama.Satariyan@etu.sorbonne-universite.fr>
2025-10-16 08:03:28 +0000

    Oct 16, 2025, 10:03 AM

--- ddpg.py
+++ ddpg.py
@@ -96,7 +96,7 @@ critic_mlp = MLP(
 critic = TDM(critic_mlp, in_keys=["observation", "action"], out_keys=["state_action_value"]) # = QValue
 
 # Target Networks
-actor_target = deepcopy(actor) # no noise in target
+actor_target = deepcopy(policy) # no noise in target
 critic_target = deepcopy(critic)
 
 # 4. DDPG loss module
