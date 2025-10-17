32ced3ea8937d70de0720cea18123376e9911a18
Sama <Sama.Satariyan@etu.sorbonne-universite.fr>
2025-10-17 15:10:26 +0000

    Oct 17, 2025, 5:10 PM

--- TD3_DDPG_TorchRL.py
+++ TD3_DDPG_TorchRL.py
@@ -29,7 +29,7 @@ from tqdm import tqdm
 
 # parameters and hyperparameters
 INIT_RAND_STEPS = 5000 
-TOTAL_FRAMES = 10_000
+TOTAL_FRAMES = 5_000
 FRAMES_PER_BATCH = 100
 OPTIM_STEPS = 10
 BUFFER_LEN = 1_000_000
@@ -340,41 +340,41 @@ def train(
     torchrl_logger.info(
         f"solved after {total_count} steps, {total_episodes} episodes and in {t1-t0}s."
     )
-    window = 200  # adjust for smoothing strength
-    smooth_bias = np.convolve(biases, np.ones(window)/window, mode='valid')
-    smooth_qvalue = np.convolve(qvalues, np.ones(window)/window, mode='valid')
+    # window = 200  # adjust for smoothing strength
+    # smooth_bias = np.convolve(biases, np.ones(window)/window, mode='valid')
+    # smooth_qvalue = np.convolve(qvalues, np.ones(window)/window, mode='valid')
     
-    save_series("biases_newtd3.csv", biases, smooth_bias, window)
-    save_series("qvalues_newtd3.csv", qvalues, smooth_qvalue, window)
+    # save_series("biases_newtd3.csv", biases, smooth_bias, window)
+    # save_series("qvalues_newtd3.csv", qvalues, smooth_qvalue, window)
     
-    smooth_returns = np.convolve(episode_returns, np.ones(window)/window, mode='valid')
-    plt.figure(figsize=(12,4))
-    plt.plot(episode_returns, label="Raw Bias", color='tab:blue', alpha=0.5)  # transparent fluctuating curve
-    plt.plot(np.arange(window-1, len(episode_returns)), smooth_returns, label="Smoothed Return", color='tab:blue', linewidth=2)
-    plt.xtitle(f"{method} – episodic return")
-    plt.title(f"{method} – episodic return")
-    plt.xlabel("Episode")
-    plt.ylabel("Return")
-    plt.tight_layout()
-    plt.show()
+    # smooth_returns = np.convolve(episode_returns, np.ones(window)/window, mode='valid')
+    # plt.figure(figsize=(12,4))
+    # plt.plot(episode_returns, label="Raw Bias", color='tab:blue', alpha=0.5)  # transparent fluctuating curve
+    # plt.plot(np.arange(window-1, len(episode_returns)), smooth_returns, label="Smoothed Return", color='tab:blue', linewidth=2)
+    # plt.xtitle(f"{method} – episodic return")
+    # plt.title(f"{method} – episodic return")
+    # plt.xlabel("Episode")
+    # plt.ylabel("Return")
+    # plt.tight_layout()
+    # plt.show()
     
     
-    plt.figure(figsize=(12,5))
-    plt.xtitle(f"Training {method} - Bias")
-    plt.plot(biases, label="Raw Bias", color='tab:blue', alpha=0.5)  # transparent fluctuating curve
-    plt.plot(np.arange(window-1, len(biases)), smooth_bias, label="Smoothed Bias", color='tab:blue', linewidth=2)
-    plt.title(f"Training {method} - TD Bias")
-    plt.title(f"Training {method} - Bias")
-    plt.xlabel("Training Steps")
-    plt.show()
+    # plt.figure(figsize=(12,5))
+    # plt.xtitle(f"Training {method} - Bias")
+    # plt.plot(biases, label="Raw Bias", color='tab:blue', alpha=0.5)  # transparent fluctuating curve
+    # plt.plot(np.arange(window-1, len(biases)), smooth_bias, label="Smoothed Bias", color='tab:blue', linewidth=2)
+    # plt.title(f"Training {method} - TD Bias")
+    # plt.title(f"Training {method} - Bias")
+    # plt.xlabel("Training Steps")
+    # plt.show()
 
-    plt.figure(figsize=(12,5))
-    plt.xtitle(f"Training {method} - Q Values")
-    plt.plot(qvalues, label="Raw q_values", color='tab:blue', alpha=0.5)  # transparent fluctuating curve
-    plt.plot(np.arange(window-1, len(qvalues)), smooth_qvalue, label="Smoothed q_values", color='tab:blue', linewidth=2)
-    plt.title(f"Training {method} - smoothed Q Values")
-    plt.xlabel("Training Steps")
-    plt.show()
+    # plt.figure(figsize=(12,5))
+    # plt.xtitle(f"Training {method} - Q Values")
+    # plt.plot(qvalues, label="Raw q_values", color='tab:blue', alpha=0.5)  # transparent fluctuating curve
+    # plt.plot(np.arange(window-1, len(qvalues)), smooth_qvalue, label="Smoothed q_values", color='tab:blue', linewidth=2)
+    # plt.title(f"Training {method} - smoothed Q Values")
+    # plt.xlabel("Training Steps")
+    # plt.show()
 
 
 
