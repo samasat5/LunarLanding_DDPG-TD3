8a0e1d10a02411ff55019d2f96738ef94fb52dde
Sama <Sama.Satariyan@etu.sorbonne-universite.fr>
2025-10-18 13:43:22 +0000

    Oct 18, 2025, 3:43 PM

--- new.py
+++ new.py
@@ -29,7 +29,7 @@ from tqdm import tqdm
 
 # parameters and hyperparameters
 INIT_RAND_STEPS = 5000 
-TOTAL_FRAMES = 20_000
+TOTAL_FRAMES = 100_000
 FRAMES_PER_BATCH = 100
 OPTIM_STEPS = 10
 BUFFER_LEN = 1_000_000
